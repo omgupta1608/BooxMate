d214a768ad7dfe6bab04b3e5d778523259a1cf49
rishidholkheria <rishidholkheria2001@gmail.com>
2020-09-19 14:18:26 +0530

    Try

--- front-end/src/components/HomePage.jsx
+++ front-end/src/components/HomePage.jsx
@@ -1,120 +1,122 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 import Navbar from "./Navbar";
 import Herobox from "./Herobox";
 import Linkbox from "./Linkbox";
 import BooksDisplay from "./BooksDisplay";
 //hello
 class HomePage extends Component {
-    state = { books: [
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educational",
-          location: "afkjbaskjb",
-        },
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educational",
-          location: "afkjbaskjb",
-        },
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educational",
-          location: "afkjbaskjb",
-        },
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educal",
-          location: "afkjbaskjb",
-        },
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educanal",
-          location: "afkjbaskjb",
-        },
-        {
-          price: 32,
-          postdate: "afasf",
-          bookname: "aasaf",
-          category: "Educnal",
-          location: "afkjbaskjb",
-        },
-      ], }
+  state = {
+    books: [
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educational",
+        location: "afkjbaskjb",
+      },
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educational",
+        location: "afkjbaskjb",
+      },
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educational",
+        location: "afkjbaskjb",
+      },
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educal",
+        location: "afkjbaskjb",
+      },
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educanal",
+        location: "afkjbaskjb",
+      },
+      {
+        price: 32,
+        postdate: "afasf",
+        bookname: "aasaf",
+        category: "Educnal",
+        location: "afkjbaskjb",
+      },
+    ],
+  };
 
-     async componentDidMount() {
-        // await Axios(
-        //   'http://localhost:3200',{
-        //     method: 'GET',
-        //     data: {
-        //       query: `{
-        //       books{
-        //         bookid
-        //         bookname
-        //         price
-        //         author
-        //       }}
-        //       `
-        //     },
-        //     headers: {
-        //       'Content-Type': "application/graphql",
-        //       "Access-Control-Allow-Origin": "*"
-        //     }
-        //   }
-        // ).then(res => res.json()).then(json => console.log(json));
+  async componentDidMount() {
+    // await Axios(
+    //   'http://localhost:3200',{
+    //     method: 'GET',
+    //     data: {
+    //       query: `{
+    //       books{
+    //         bookid
+    //         bookname
+    //         price
+    //         author
+    //       }}
+    //       `
+    //     },
+    //     headers: {
+    //       'Content-Type': "application/graphql",
+    //       "Access-Control-Allow-Origin": "*"
+    //     }
+    //   }
+    // ).then(res => res.json()).then(json => console.log(json));
 
-        // axios({
-        //   method: 'get',
-        //   url:'http://localhost:3200/graphql',
-        //   headers: {          }, 
-        //   data: {
-        //     query: `{
-        //       query books{
-        //         bookid
-        //         bookname
-        //         price
-        //         author
-        //       }
-        //     }`
-        //   }
-        // }).then(json => console.log(json));
-        var response = await axios.get("http://localhost:4640/api/books",{
-          data: {
-                 query: `{
+    // axios({
+    //   method: 'get',
+    //   url:'http://localhost:3200/graphql',
+    //   headers: {          },
+    //   data: {
+    //     query: `{
+    //       query books{
+    //         bookid
+    //         bookname
+    //         price
+    //         author
+    //       }
+    //     }`
+    //   }
+    // }).then(json => console.log(json));
+    const { data: books } = await axios.get("http://localhost:4640/api/books", {
+      data: {
+        query: `{
                  query books{
-                     bookid
+                    bookid
                     bookname
                     price
-                    author
+                    
                 }
-                }`
-          },
-          headers: {
-            'Content-Type': "application/json"
-          }
-        });
-        console.log(response);
-      }
+                }`,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    this.setState({ books })
+  }
 
-    render() { 
-        return ( 
-            <div className="App">
-            <Navbar />
-            <Herobox />
-            <Linkbox />
-            <BooksDisplay books={this.state.books}></BooksDisplay>
-          </div>
-         );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Herobox />
+        <Linkbox />
+        <BooksDisplay books={this.state.books}></BooksDisplay>
+      </div>
+    );
+  }
 }
- 
+
 export default HomePage;
