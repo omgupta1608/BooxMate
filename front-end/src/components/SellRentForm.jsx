b0493d738f2dab7233ea85fe127145fdb084468c
sahilchalia <sahilchalia20@gmail.com>
2020-09-19 10:24:35 +0530

    image file input and radio button  added

--- front-end/src/components/SellRentForm.jsx
+++ front-end/src/components/SellRentForm.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 export class SellRentForm extends Component {
+	
+	myfunction() {
+		document.getElementById("rent-only").style.display='none'
+		document.getElementById("rent-only1").style.display='none'
+	}
+
+	myfunction1() {
+		document.getElementById("rent-only").style.display='block'
+		document.getElementById("rent-only1").style.display='block'
+	}
+	
 	render() {
 		return (
 			<div className="sell-rent-form">
@@ -20,18 +31,18 @@ export class SellRentForm extends Component {
 					</select>
 					<input type="text" name="location" placeholder="Your Location (State/City)" />
 					<div className="radio-wrapper">
-						<input className="sellradio" type="radio" name="radio" id="" />
+						<input onClick={this.myfunction} className="sellradio" type="radio" name="radio" id="sellradio" />
 						<label htmlFor="sell-radio">Sell</label>
-						<input className="rentradio" type="radio" name="radio" id="" />
+						<input onClick={this.myfunction1} className="rentradio" type="radio" name="radio" id="rentradio" />
 						<label htmlFor="rent-radio">Rent</label>
+						<input type="file" name="bookimage" id="bookimage" accept="image/*" />
 					</div>
 					<input type="text" name="bookmrp" id="" placeholder="M.R.P of the Book" />
 					<input type="text" name="bookprice" id="" placeholder="Price of the Book" />
-					<input type="text" name="rent-duration" placeholder="Duration of the Renting period (Skip if you are selling your book)" />
-					<input type="text" name="security-deposit" placeholder="Amount of Security deposit (Skip if you are selling your book)" />
+					<input id="rent-only" type="text" name="rent-duration" placeholder="Duration of the Renting period" />
+					<input id="rent-only1" type="text" name="security-deposit" placeholder="Amount of Security deposit" />
 					<input className="submit" type="submit" value="Submit" />
 				</form>
-
 			</div>
 		)
 	}
